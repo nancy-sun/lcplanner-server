6e6c90396cce064cf2e300f73d19c52aff00a13c
Nancy Sun <99620863+nancy-sun@users.noreply.github.com>
2022-09-05 20:37:14 -0700

    add progress calculation

--- index.js
+++ index.js
@@ -213,7 +213,15 @@ const resolvers = {
 
     TasksList: {
         id: ({ _id, id }) => _id || id, // id as _id in mongoDB
-        progress: () => 0,
+        progress: async ({ _id }, _, { db }) => {
+            const tasks = await db.collection("Tasks").find({ tasksListID: _id }).toArray();
+            const completed = tasks.filter(task => task.isCompleted);
+            if (tasks.length === 0) {
+                return 0;
+            }
+            const progression = (completed.length / tasks.length) * 100;
+            return progression;
+        },
         access: async ({ accessIDs }, _, { db }) => Promise.all(  // connect with Users collection
             accessIDs.map((accessID) => db.collection("Users").findOne({ _id: accessID }))
         ),
