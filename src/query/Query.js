7a838b1e819d5f1248f3949ad7e1f00744b9999e
Nancy Sun <99620863+nancy-sun@users.noreply.github.com>
2022-09-05 23:22:50 -0700

    refactor query

--- src/query/Query.js
+++ src/query/Query.js
@@ -1,21 +1,10 @@
-const { ObjectId } = require("mongodb");
+
+const myTasksList = require("./tasksListQuery").myTasksList;
+const getTasksList = require("./tasksListQuery").getTasksList;
 
 const Query = {
-    myTasksList: async (_, __, { db, user }) => { //get all tasks list under a user
-        if (!user) {
-            throw new Error("Authentication failed.");
-        }
-        return await db.collection("TasksList").find({ accessIDs: user._id }).toArray();
-
-    },
-
-    getTasksList: async (_, { id }, { db, user }) => { // get a single tasks list
-        if (!user) {
-            throw new Error("Authentication failed.");
-        }
-        const foundTasksList = await db.collection("TasksList").findOne({ _id: ObjectId(id) });
-        return foundTasksList;
-    },
+    myTasksList,
+    getTasksList
 };
 
 module.exports = Query;
---
+++ src/query/tasksListQuery.js
@@ -0,0 +1,18 @@
+const { ObjectId } = require("mongodb");
+
+const myTasksList = async (_, __, { db, user }) => { //get all tasks list under a user
+    if (!user) {
+        throw new Error("Authentication failed.");
+    }
+    return await db.collection("TasksList").find({ accessIDs: user._id }).toArray();
+}
+
+const getTasksList = async (_, { id }, { db, user }) => { // get a single tasks list
+    if (!user) {
+        throw new Error("Authentication failed.");
+    }
+    const foundTasksList = await db.collection("TasksList").findOne({ _id: ObjectId(id) });
+    return foundTasksList;
+};
+
+module.exports = { myTasksList, getTasksList };
