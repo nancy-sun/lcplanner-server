bff6fa8095ce3684b4d80f3e785a540ad70224d5
Nancy Sun <99620863+nancy-sun@users.noreply.github.com>
2022-10-09 21:36:16 -0700

    remove avatar from mutation and db, avatar will be stored on s3

--- src/mutation/Mutation.js
+++ src/mutation/Mutation.js
@@ -1,14 +1,11 @@
 const { signUp, signIn } = require("./authMutation");
 const { createTasksList, updateTasksList, deleteTasksList, addTasksListUser } = require("./tasksListMutation");
 const { createTask, updateTask, deleteTask } = require("./taskMutation");
-const { updateAvatar } = require("./userMutation");
 
 const Mutation = {
     /* auth */
     signUp,
     signIn,
-
-    updateAvatar,
 
     /* tasks list */
     createTasksList,
